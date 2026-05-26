89ed5a4679bfbd5384a71570bb2d30912b8f89bf
H. Cumaili <kholioosm@gmail.com>
2026-05-27 02:15:38 +0300

    Update menu.js

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,1 +1,27 @@
+const menuToggle =
+document.querySelector(
+  '.menu-toggle'
+);
 
+const menuPanel =
+document.querySelector(
+  '.menu-panel'
+);
+
+if(
+  menuToggle &&
+  menuPanel
+){
+
+  menuToggle.addEventListener(
+    'click',
+    () => {
+
+      menuPanel.classList.toggle(
+        'active'
+      );
+
+    }
+  );
+
+}

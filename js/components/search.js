86a1c84b25b19ddbf08cb903b859a67f8af9cfa4
H. Cumaili <kholioosm@gmail.com>
2026-05-26 06:45:49 +0300

    Update search.js

--- js/components/search.js
+++ js/components/search.js
@@ -1,1 +1,30 @@
+const searchToggle =
+document.querySelector('.search-toggle');
 
+const searchPanel =
+document.querySelector('.search-panel');
+
+const searchClose =
+document.querySelector('.search-close');
+
+
+searchToggle.addEventListener(
+'click',
+() => {
+
+  searchPanel.classList.add(
+    'active'
+  );
+
+});
+
+
+searchClose.addEventListener(
+'click',
+() => {
+
+  searchPanel.classList.remove(
+    'active'
+  );
+
+});

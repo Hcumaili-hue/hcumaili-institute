5f539b7caeda6aea058defc29ff79d04d59a9033
H. Cumaili <kholioosm@gmail.com>
2026-06-03 05:29:12 +0300

    Implement navbar scroll state functionality
    Add scroll event listener to change navbar style

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,1 +1,30 @@
+/* =========================
+NAVBAR SCROLL STATE
+========================= */
 
+const header =
+document.querySelector(
+  ".site-header"
+);
+
+window.addEventListener(
+  "scroll",
+  () => {
+
+    if(
+      window.scrollY > 80
+    ){
+
+      header.classList.add(
+        "scrolled"
+      );
+
+    }else{
+
+      header.classList.remove(
+        "scrolled"
+      );
+    }
+
+  }
+);
